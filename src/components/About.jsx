4f0fcbad39dcbb9be000a38754f51041edb27c79
Inna Poliakova <polyakova1081@gmail.com>
2023-06-14 21:06:29 +0100

    change About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,9 @@
 const About = () => {
   return (
-
-      <div className="about">
-        <p>Project's goal</p>
-      </div>
-  
+    <div className="about">
+      <p>Project's goal</p>
+      <p>Project's description</p>
+    </div>
   );
 };
 
